ff5d7140e5b9d6e62c89c66ef439cd4f1fa04616
Alibek-dev <54788352+Alibek-dev@users.noreply.github.com>
2020-09-28 11:27:09 +0600

    added views

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {layout: 'main'},
     component: () => import('../views/Home')
   },
   {
@@ -16,11 +17,47 @@ const routes = [
     component: () => import('../views/Login')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: {layout: 'main'},
     component: () => import('../views/Categories')
-  }
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Detail-record')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record')
+  },
 ]
 
 const router = new VueRouter({
