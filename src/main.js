87b02d6a8ad271f2ab1d3589b7ef00a44cd82287
Alibek-dev <54788352+Alibek-dev@users.noreply.github.com>
2020-09-29 23:05:17 +0600

    added validations to registration form

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
+import messagePlugin from './utils/message.plugin'
 import 'materialize-css/dist/js/materialize.min'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+
 Vue.filter('date', dateFilter)
 Vue.use(Vuelidate)
+Vue.use(messagePlugin)
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -55,6 +55,7 @@
 
 <script>
     import {email, required} from 'vuelidate/lib/validators'
+    import messages from "../utils/messages";
     export default {
         name: 'Login',
         data: () => ({
@@ -65,6 +66,13 @@
             email: {email, required},
             password: {required},
         },
+
+        mounted() {
+            if (messages[this.$route.query.message]) {
+                this.$message(messages[this.$route.query.message])
+            }
+        },
+
         methods: {
             submitHandler() {
                 if (this.$v.$invalid) {
